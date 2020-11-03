382a5d66b0d1b4fe2f3de1b1fe932f4e178247da
ConnorHalford <connor.halford@hotmail.co.uk>
2020-11-03 00:16:47 +0000

    Billboarding sprites and projecting 'shadow' sprites downwards

---
+++ game-off-2020/Assets/Code/Billboard.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+	private Camera _camera = null;
+
+	private void LateUpdate()
+	{
+		if (_camera == null)
+		{
+			_camera = Camera.main;
+		}
+		if (_camera != null)
+		{
+			//Vector3 forward = transform.position - _camera.transform.position;
+			//transform.rotation = Quaternion.LookRotation(forward, Vector3.up);
+			transform.rotation = _camera.transform.rotation;
+		}
+	}
+}
---
+++ game-off-2020/Assets/Code/ShadowSprite.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class ShadowSprite : MonoBehaviour
+{
+	[SerializeField] private Color _shadowColor = new Color(0.0f, 0.0f, 0.0f, 0.7f);
+
+	private SpriteRenderer _baseRenderer = null;
+	private SpriteRenderer _shadowRenderer = null;
+
+	private static int _layerMask = 0;
+
+	private void Awake()
+	{
+		_baseRenderer = GetComponent<SpriteRenderer>();
+
+		GameObject shadowGO = new GameObject(name + " Shadow");
+		shadowGO.transform.parent = transform;
+		_shadowRenderer = shadowGO.AddComponent<SpriteRenderer>();
+		_shadowRenderer.color = _shadowColor;
+		_shadowRenderer.sortingOrder = _baseRenderer.sortingOrder - 1;
+
+		_layerMask = LayerMask.GetMask("Environment");
+	}
+
+	private void LateUpdate()
+	{
+		if (_baseRenderer == null || _shadowRenderer == null)
+		{
+			return;
+		}
+		_shadowRenderer.sprite = _baseRenderer.sprite;
+
+		Vector3 position = transform.position;
+		if (Physics.Raycast(position, Vector3.down, out RaycastHit hit, float.PositiveInfinity, _layerMask))
+		{
+			position = hit.point;
+		}
+		_shadowRenderer.transform.position = position + 0.01f * Vector3.up;
+
+		Vector3 rotation = transform.rotation.eulerAngles;
+		_shadowRenderer.transform.localEulerAngles = new Vector3(90.0f - rotation.x, 0.0f, 0.0f);
+	}
+}
