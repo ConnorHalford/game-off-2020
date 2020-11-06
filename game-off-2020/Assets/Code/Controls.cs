fc767305d2d074d5eb63e15418ae938c84d5a426
ConnorHalford <connor.halford@hotmail.co.uk>
2020-11-06 21:48:51 +0000

    Player ground movement

---
+++ game-off-2020/Assets/Code/Controls.cs
@@ -0,0 +1,187 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Input/Controls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Controls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Controls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Controls"",
+    ""maps"": [
+        {
+            ""name"": ""Character"",
+            ""id"": ""b24e14b8-2f4d-4a46-b1c9-3496c845f309"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""36248878-78aa-40c0-8c01-7d057ed80382"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""d42299f8-2940-42ef-9694-8b0b3b97b49f"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""Keyboard"",
+                    ""id"": ""fd6c768b-9d9d-49f8-ba50-4b452b4b3d7d"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""0ced49b1-084d-405e-a5d5-15ea2dc7f803"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""0c0c1e29-1954-4d1b-be02-126c6ee5731f"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""d819878a-02f1-4bc2-9cfe-9ae005dbdbc9"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""5d3fe972-f2cd-40f2-8758-c0c8d9505c28"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Character
+        m_Character = asset.FindActionMap("Character", throwIfNotFound: true);
+        m_Character_Movement = m_Character.FindAction("Movement", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Character
+    private readonly InputActionMap m_Character;
+    private ICharacterActions m_CharacterActionsCallbackInterface;
+    private readonly InputAction m_Character_Movement;
+    public struct CharacterActions
+    {
+        private @Controls m_Wrapper;
+        public CharacterActions(@Controls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Character_Movement;
+        public InputActionMap Get() { return m_Wrapper.m_Character; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(CharacterActions set) { return set.Get(); }
+        public void SetCallbacks(ICharacterActions instance)
+        {
+            if (m_Wrapper.m_CharacterActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_CharacterActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_CharacterActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_CharacterActionsCallbackInterface.OnMovement;
+            }
+            m_Wrapper.m_CharacterActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+            }
+        }
+    }
+    public CharacterActions @Character => new CharacterActions(this);
+    public interface ICharacterActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+    }
+}
---
+++ game-off-2020/Assets/Code/Player.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField] private SpriteRenderer _sprite = null;
+	[SerializeField] private float _moveSpeed = 3.0f;
+
+	private Controls _controls = null;
+	private Camera _camera = null;
+
+	private void Awake()
+	{
+		_controls = new Controls();
+	}
+
+	private void OnEnable()
+	{
+		_controls.Character.Enable();
+	}
+
+	private void OnDisable()
+	{
+		_controls.Character.Disable();
+	}
+
+	private void Update()
+	{
+		if (_camera == null)
+		{
+			_camera = Camera.main;
+		}
+
+		// We want 'forward' and 'backward' to be up and down the screen, but since it's a perspective
+		// camera we have to create a screenspace up vector then project it onto a flat plane
+		Plane ground = new Plane(Vector3.up, transform.position);
+		Vector3 screenPos = _camera.WorldToScreenPoint(transform.position);
+		Ray ray = _camera.ScreenPointToRay(screenPos);
+		ground.Raycast(ray, out float entryDistance);
+		Vector3 posA = ray.origin + entryDistance * ray.direction;
+
+		ray = _camera.ScreenPointToRay(screenPos + Vector3.up);		// Screenspace up vector
+		ground.Raycast(ray, out entryDistance);
+		Vector3 posB = ray.origin + entryDistance * ray.direction;
+
+		Vector3 forward = (posB - posA).normalized;		// Screenspace up vector projected to world space
+		Vector3 right = _camera.transform.right;
+
+		Vector2 input = _controls.Character.Movement.ReadValue<Vector2>() * _moveSpeed * Time.deltaTime;
+		Vector3 movement = input.x * right + input.y * forward;
+		transform.position += movement;
+	}
+}
